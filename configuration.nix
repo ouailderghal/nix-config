49b8fdc566fbf41891631309ab563e214188a607
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-04 12:02:16 +0100

    Add pass and browserpass to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -108,7 +108,11 @@
     git
     tmux
     tree
+    pass
   ];
+
+  # Enable BrowserPass
+  programs.browserpass.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are started in user sessions.
   programs.mtr.enable = true;
