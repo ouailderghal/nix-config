7c7072ab36fd74d41687bf8b8b612716c738be38
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-04 09:04:20 +0100

    Add home/default.nix

---
+++ home/default.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ...}:
+{
+  imports = [
+    ./alacritty
+    ./tmux
+    ./zsh
+    ./i3
+    ./git
+    ./lf
+  ];
+    
+  home.username = "ouail";
+  home.homeDirectory = "/home/ouail";
+
+  home.packages = with pkgs; [
+    gh
+    brave
+    tree
+    discord
+  
+    (pkgs.nerdfonts.override { 
+      fonts = [ "UbuntuMono" ]; 
+    })
+  ];
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "23.11";
+}
