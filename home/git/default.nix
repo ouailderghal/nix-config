8799c7ddc6592a547c1fd1b81bca5465708b9623
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-04 14:27:34 +0100

    Add signing keys to home/git

--- home/git/default.nix
+++ home/git/default.nix
@@ -2,8 +2,14 @@
 {
   programs.git = {
     enable = true;
+
     userName = "Ouail Derghal";
     userEmail = "ouailderghal1@gmail.com";
+
+    signing = {
+      key = "C9557C30943B96D2C27F18E61F8E47CECB0E0E16";
+      signByDefault = true;
+    };
 
     extraConfig = {
       init = {
@@ -19,13 +25,14 @@
       {
         contents = {
           user = {
+            name = "Ouail Derghal";
             email = "ouail.derghal@imt-atlantique.fr";
+            signingKey = "2E3EB06AF98BB2D7B2533322251152B212939FA3";
           };
         };
 
         condition = "gitdir:~/Projects/imt/";
       }
     ];
-  
   };
 }
