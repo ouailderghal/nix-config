86f690328fcc8f06c616653bba7d0fd1652d6d25
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-04 09:03:46 +0100

    Add configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,128 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname.
+  networking.hostName = "tokyo"; 
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver = {
+    enable = true;
+
+    layout = "fr";
+    xkbVariant = "latin9";
+
+    displayManager = {
+      lightdm.enable = true;
+      defaultSession = "none+i3";
+    };
+
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+
+      extraPackages = with pkgs; [
+        dmenu
+        i3lock
+        i3blocks
+        pavucontrol
+        arandr
+        copyq
+        nitrogen
+     ];
+    };
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ouail = {
+    isNormalUser = true;
+    description = "Ouail Derghal";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    gcc
+    nodejs
+    go
+    gopls
+    python3
+    neovim
+    zsh
+    wget
+    git
+    tmux
+    tree
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.lightdm.enableGnomeKeyring = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = false;
+
+  system.stateVersion = "23.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
