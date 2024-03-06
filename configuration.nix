9e190fc7092f4af13b98b6ed837acf17607101a4
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-06 20:50:01 +0100

    Remove redundant xorg config from configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -34,37 +34,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  services.xserver = {
-    enable = true;
-
-    layout = "fr";
-    xkbVariant = "latin9";
-
-    displayManager = {
-      lightdm.enable = true;
-      defaultSession = "none+i3";
-    };
-
-    desktopManager = {
-      xterm.enable = false;
-      xfce.enable = true;
-    };
-
-    windowManager.i3 = {
-      enable = true;
-
-      extraPackages = with pkgs; [
-        dmenu
-        i3lock
-        i3blocks
-        pavucontrol
-        arandr
-        copyq
-        nitrogen
-     ];
-    };
-  };
-
   # Configure console keymap
   console.keyMap = "fr";
 
