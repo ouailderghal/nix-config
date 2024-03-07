25cb73fe5510c12bebedbfef368e49756444ac11
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-07 11:54:55 +0100

    alacritty: update font and colors

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -7,9 +7,9 @@
       shell.program = "zsh";
 
       font = rec {
-        normal.family = "UbuntuMono Nerd Font";
+        normal.family = "JetBrainsMono NFM Medium";
         size = 11;
-        bold = { style = "Bold"; };
+        bold = { style = "Medium"; };
       };
 
       window.padding = {
@@ -47,30 +47,30 @@
 
       colors = {
         primary = {
-          background = "0x1e1e1e";
-          foreground = "0xffffff";
+          background = "0xfbf1c7";
+          foreground = "0x3c3836";
         };
 
         normal = {
-          black = "0x171421";
-          red = "0xc01c28";
-          green = "0x26a269";
-          yellow = "0xa2734c";
-          blue = "0x12488b";
-          magenta = "0xa347ba";
-          cyan = "0x2aa1b3";
-          white = "0xd0cfcc";
+          black = "0xfbf1c7";
+          red = "0xcc241d";
+          green = "0x98971a";
+          yellow = "0xd79921";
+          blue = "0x458588";
+          magenta = "0xb16286";
+          cyan = "0x689d6a";
+          white = "0x7c6f64";
         };
 
         bright = {
-          black = "0x535c64";
-          red = "0xf66151";
-          green = "0x33d17a";
-          yellow = "0xe9ad0c";
-          blue = "0x2a7bde";
-          magenta = "0xc061cb";
-          cyan = "0x33c7de";
-          white = "0xffffff";
+          black = "0x928374";
+          red = "0x9d0006";
+          green = "0x79740e";
+          yellow = "0xb57614";
+          blue = "0x076678";
+          magenta = "0x8f3f71";
+          cyan = "0x427b58";
+          white = "0x3c3836";
         };
       };
     };
