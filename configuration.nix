aa6945a0acfd7d3ea65a9907ea572f0c0b4792bc
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-07 12:52:12 +0100

    Add touchpad support to xorg.nix module

--- configuration.nix
+++ configuration.nix
@@ -8,27 +8,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr";
-
   # Enable CUPS to print documents.
   services.printing.enable = false;
 
@@ -42,10 +21,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.touchpad.disableWhileTyping = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ouail = {
--- modules/xorg.nix
+++ modules/xorg.nix
@@ -6,16 +6,25 @@
     layout = "fr";
     xkbVariant = "latin9";
 
+    # Enable LightDM display manager.
     displayManager = {
       lightdm.enable = true;
       defaultSession = "none+i3";
     };
 
+    # Enable XFCE desktop.
     desktopManager = {
       xterm.enable = false;
       xfce.enable = true;
     };
 
+    # Enable touchpad support.
+    libinput = {
+      enable = true;
+      touchpad.disableWhileTyping = true;
+    };
+
+    # Enable i3 window manager and install additional packages.
     windowManager.i3 = {
       enable = true;
 
