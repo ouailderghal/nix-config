0d3e0a16a7381ea7b0be330428298bcedbb1221b
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-07 13:55:15 +0100

    Add sound.nix module

--- configuration.nix
+++ configuration.nix
@@ -11,17 +11,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = false;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ouail = {
     isNormalUser = true;
--- modules/default.nix
+++ modules/default.nix
@@ -5,5 +5,6 @@
     ./virtualisation.nix
     ./bootloader.nix
     ./locale.nix
+    ./sound.nix
   ];
 }
