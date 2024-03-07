0480f544bb79854d3cfaecb76393c4903d649e76
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-07 11:55:06 +0100

    neomutt: remove fetch from git action

--- home/neomutt/default.nix
+++ home/neomutt/default.nix
@@ -1,15 +1,4 @@
 { config, pkgs, ... }:
-let
-  config = builtins.fetchGit {
-    url = "https://gitlab.com/ouailderghal1/neomutt.git";
-    rev = "dc7c064bc3716e013c6376ab13d981c72623dc72";
-  };
-in 
 {
   programs.neomutt.enable = true;
-
-  xdg.configFile.neomutt = {
-    source = config;
-    recursive = true;
-  };
 }
