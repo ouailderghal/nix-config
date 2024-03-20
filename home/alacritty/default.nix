a365f28d7a95eaa6348b4e61a2478ac74d6c05f1
Ouail Derghal <ouailderghal1@gmail.com>
2024-03-20 13:39:21 +0100

    Update alacritty theme

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,60 +1,31 @@
 { config, pkgs, ...}:
 let
-  gruvboxLight = {
+  theme = {
     primary = {
-      background = "0xfbf1c7";
-      foreground = "0x3c3836";
+      background = "0x1e1e1e";
+      foreground = "0xffffff";
     };
 
     normal = {
-      black = "0xfbf1c7";
-      red = "0xcc241d";
-      green = "0x98971a";
-      yellow = "0xd79921";
-      blue = "0x458588";
-      magenta = "0xb16286";
-      cyan = "0x689d6a";
-      white = "0x7c6f64";
+      black = "0x171421";
+      red = "0xc01c28";
+      green = "0x26a269";
+      yellow = "0xa2734c";
+      blue = "0x12488b";
+      magenta = "0xa347ba";
+      cyan = "0x2aa1b3";
+      white = "0xd0cfcc";
     };
 
     bright = {
-      black = "0x928374";
-      red = "0x9d0006";
-      green = "0x79740e";
-      yellow = "0xb57614";
-      blue = "0x076678";
-      magenta = "0x8f3f71";
-      cyan = "0x427b58";
-      white = "0x3c3836";
-    };
-  };
-
-  gruvboxDark = {
-    primary = {
-      background = "0x282828";
-      foreground = "0xebdbb2";
-    };
-
-    normal = {
-      black = "0x282828";
-      red = "0xcc241d";
-      green = "0x98971a";
-      yellow = "0xd79921";
-      blue = "0x458588";
-      magenta = "0xb16286";
-      cyan = "0x689d6a";
-      white = "0xa89984";
-    };
-
-    bright = {
-      black = "0x928374";
-      red = "0xfb4934";
-      green = "0xb8bb26";
-      yellow = "0xfabd2f";
-      blue = "0x83a598";
-      magenta = "0xd3869b";
-      cyan = "0x8ec07c";
-      white = "0xebdbb2";
+      black = "0x535c64";
+      red = "0xf66151";
+      green = "0x33d17a";
+      yellow = "0xe9ad0c";
+      blue = "0x2a7bde";
+      magenta = "0xc061cb";
+      cyan = "0x33c7de";
+      white = "0xffffff";
     };
   };
 in
@@ -65,7 +36,7 @@ in
     settings = {
       shell.program = "zsh";
 
-      colors = gruvboxDark;
+      colors = theme;
 
       font = rec {
         normal.family = "UbuntuMono Nerd Font";
